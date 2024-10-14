fcd7bbf644504a6d5d185de540e63e35ed20c1c5
WilliamHeidemann <Williamskou2227@gmail.com>
2024-10-14 11:55:07 +0200

    remove unused code

--- Assets/Scripts/Runtime/Models/Supply.cs
+++ Assets/Scripts/Runtime/Models/Supply.cs
@@ -17,26 +17,10 @@ namespace Runtime.Models
             _energy = energy;
             _mechanical = mechanical;
         }
-
+        
         public int Blood => _blood;
         public int Energy => _energy;
         public int Mechanical => _mechanical;
-
-        public static Supply Default(Model model)
-        {
-            return model switch
-            {
-                Model.ConnectorBox => ConnectorBox,
-                Model.ConnectionSlot => None,
-                Model.Cogs => Cogs,
-                Model.Pipes => Pipes,
-                Model.Heart => Heart,
-                Model.Eyes => Eyes,
-                Model.Tentacle => Tentacle,
-                Model.Wings => Wings,
-                _ => throw new ArgumentOutOfRangeException(nameof(model), model, null)
-            };
-        }
 
         public Supply WithVariation()
         {
@@ -45,14 +29,5 @@ namespace Runtime.Models
             _mechanical += Random.Range(-1, 2);
             return this;
         }
-        
-        public static readonly Supply None = new(blood: 0, energy: 0, mechanical: 0);
-        private static readonly Supply ConnectorBox = new(blood: 0, energy: 0, mechanical: -4);
-        private static readonly Supply Cogs = new(blood: 0, energy: 1, mechanical: 1);
-        private static readonly Supply Pipes = new(blood: 0, energy: 0, mechanical: 3);
-        private static readonly Supply Heart = new(blood: 5, energy: -3, mechanical: 0);
-        private static readonly Supply Eyes = new(blood: 1, energy: 2, mechanical: 0);
-        private static readonly Supply Tentacle = new(blood: 4, energy: -3, mechanical: 0);
-        private static readonly Supply Wings = new(blood: -6, energy: 9, mechanical: 0);
     }
 }
