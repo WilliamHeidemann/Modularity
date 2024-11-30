3b8d427b0445869fb04749f606c527adbcf3e697
William Skou Heidemann <williamskou2227@gmail.com>
2024-11-30 10:48:50 +0100

    remove unused using statements

--- Assets/Scripts/Runtime/Scriptable Objects/AutoSpawner.cs
+++ Assets/Scripts/Runtime/Scriptable Objects/AutoSpawner.cs
@@ -2,8 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using log4net.DateFormatter;
-using NUnit.Framework;
 using Runtime.Backend;
 using Runtime.Components;
 using Runtime.Components.Segments;
