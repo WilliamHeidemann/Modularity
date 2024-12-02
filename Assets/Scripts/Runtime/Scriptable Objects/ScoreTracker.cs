a77e79021d5dc21cfd3b3b8348bf6b0aa3814ce7
William Skou Heidemann <williamskou2227@gmail.com>
2024-12-02 09:50:49 +0100

    update line indents

--- Assets/Scripts/Runtime/Scriptable Objects/ScoreTracker.cs
+++ Assets/Scripts/Runtime/Scriptable Objects/ScoreTracker.cs
@@ -1,76 +1,75 @@
-using UnityEngine;
-using System;
-using DG.Tweening;
-using Runtime.Components.Segments;
-using Runtime.Components.Utility;
-using Runtime.Scriptable_Objects;
-using UnityEngine.Serialization;
-using UnityUtils;
-using UtilityToolkit.Runtime;
 using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
+using UnityEngine;
 
-[CreateAssetMenu(fileName = "ScoreTracker", menuName = "ScoreTracker")]
-public class ScoreTracker : ScriptableObject
+namespace Runtime.Scriptable_Objects
 {
-    [SerializeField] private int _heartsConnected;
-    [SerializeField] private int _furnacesConnected;
-    [SerializeField] private int _brainsActivated;
-    [SerializeField] private int _enginesActivated;
-    [SerializeField] private int _hybridsActivated;
-    [SerializeField] private int _energySpheresCollected;
-    void OnEnable()
+    [CreateAssetMenu(fileName = "ScoreTracker", menuName = "ScoreTracker")]
+    public class ScoreTracker : ScriptableObject
     {
-        ResetScore();
-        FlowControl.OnProducerActivated += ProducerActivation;
-        FlowControl.OnSourcesLinkedCheck += UpdateSourceScore;
-        AutoSpawner.OnCollectedCollectables += Collected;
-    }
-    void OnDisable()
-    {
-        FlowControl.OnProducerActivated -= ProducerActivation;
-        FlowControl.OnSourcesLinkedCheck -= UpdateSourceScore;
-        AutoSpawner.OnCollectedCollectables -= Collected;
+        [SerializeField] private int _heartsConnected;
+        [SerializeField] private int _furnacesConnected;
+        [SerializeField] private int _brainsActivated;
+        [SerializeField] private int _enginesActivated;
+        [SerializeField] private int _hybridsActivated;
+        [SerializeField] private int _energySpheresCollected;
 
-    }
-    private void ProducerActivation(StaticSegmentData staticSegmentData)
-    {
-        if (staticSegmentData.IsBlood && staticSegmentData.IsSteam)
+        private void OnEnable()
         {
-            _hybridsActivated += 1;
+            ResetScore();
+            FlowControl.OnProducerActivated += ProducerActivation;
+            FlowControl.OnSourcesLinkedCheck += UpdateSourceScore;
+            AutoSpawner.OnCollectedCollectables += Collected;
         }
-        else if (staticSegmentData.IsBlood)
+
+        private void OnDisable()
         {
-            _brainsActivated += 1;
+            FlowControl.OnProducerActivated -= ProducerActivation;
+            FlowControl.OnSourcesLinkedCheck -= UpdateSourceScore;
+            AutoSpawner.OnCollectedCollectables -= Collected;
         }
-        else _enginesActivated +=1;
-    }
-    private void UpdateSourceScore(HashSet<SegmentData> sources)
-    {
-        var blood = 0;
-        var steam = 0;
-        foreach (var source in sources)
+
+        private void ProducerActivation(StaticSegmentData staticSegmentData)
         {
-            if (source.StaticSegmentData.IsBlood)
+            if (staticSegmentData.IsBlood && staticSegmentData.IsSteam)
             {
-                blood += 1;
+                _hybridsActivated += 1;
             }
-            else steam += 1;
+            else if (staticSegmentData.IsBlood)
+            {
+                _brainsActivated += 1;
+            }
+            else _enginesActivated += 1;
         }
-        _heartsConnected = blood;
-        _furnacesConnected = steam;
-    }
-    private void Collected(int noCollected)
-    {
-        _energySpheresCollected += noCollected;
-    }
-    private void ResetScore()
-    {
-        _heartsConnected = 0;
-        _furnacesConnected = 0;
-        _brainsActivated = 0;
-        _enginesActivated = 0;
-        _energySpheresCollected = 0;
-    }
 
+        private void UpdateSourceScore(HashSet<SegmentData> sources)
+        {
+            var blood = 0;
+            var steam = 0;
+            foreach (var source in sources)
+            {
+                if (source.StaticSegmentData.IsBlood)
+                {
+                    blood += 1;
+                }
+                else steam += 1;
+            }
+
+            _heartsConnected = blood;
+            _furnacesConnected = steam;
+        }
+
+        private void Collected(int noCollected)
+        {
+            _energySpheresCollected += noCollected;
+        }
+
+        private void ResetScore()
+        {
+            _heartsConnected = 0;
+            _furnacesConnected = 0;
+            _brainsActivated = 0;
+            _enginesActivated = 0;
+            _energySpheresCollected = 0;
+        }
+    }
 }
