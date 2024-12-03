59a90d94b2606a3f91bb29b250be72bd0c31f041
TimmyTheAverage <90619736+TimmyTheAverage@users.noreply.github.com>
2024-12-03 19:27:27 +0100

    Options slides has been made much more responsive

--- Assets/Scripts/Runtime/Components/OptionMenuController.cs
+++ Assets/Scripts/Runtime/Components/OptionMenuController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,9 +9,18 @@ namespace Runtime.Components
         [HideInInspector] public GameObject _menuToReturnTo;
         [SerializeField] private Slider _musicSlider;
         [SerializeField] private Slider _SFXSlider;
+        [SerializeField] private Image _musicCrossout;
+        [SerializeField] private Image _SFXCrossout;
+        [SerializeField] private AudioSource _testingAudioSource;
 
         public delegate void SoundChange(float musicVolume, float SFXVolume);
         public static event SoundChange OnSoundChange;
+
+        private void Start()
+        {
+            _testingAudioSource = GetComponent<AudioSource>();
+            _testingAudioSource.volume = _SFXSlider.value;
+        }
 
         public void Return()
         {
@@ -21,11 +31,38 @@ namespace Runtime.Components
         public void OnMusicVolumeChanged()
         {
             OnSoundChange?.Invoke(_musicSlider.value, _SFXSlider.value);
+
+            if (_musicSlider.value == 0)
+            {
+                _musicCrossout.gameObject.SetActive(true);
+            }
+            else
+            {
+                _musicCrossout.gameObject.SetActive(false);
+            }
         }
 
         public void OnSFXVolumeChanged()
         {
+            StopAllCoroutines();
             OnSoundChange?.Invoke(_musicSlider.value, _SFXSlider.value);
+            _testingAudioSource.volume = _SFXSlider.value;
+
+            if (_SFXSlider.value == 0)
+            {
+                _SFXCrossout.gameObject.SetActive(true);
+            }
+            else
+            {
+                StartCoroutine(PlaySound());
+                _SFXCrossout.gameObject.SetActive(false);
+            }
+        }
+
+        IEnumerator PlaySound()
+        {
+            yield return new WaitForSeconds(0.1f);
+            _testingAudioSource.Play();
         }
     }
 }
