cdf3dfde966f9000840131b536ac747a58b0392f
Dexter Reese <Ehlers.Jakob@gmail.com>
2024-12-06 12:11:28 +0100

    Signed-off-by: Dexter Reese <Ehlers.Jakob@gmail.com> made a method use a more generalised method so I could delete some code

--- Assets/Scripts/Runtime/Scriptable Objects/EndGame.cs
+++ Assets/Scripts/Runtime/Scriptable Objects/EndGame.cs
@@ -10,14 +10,14 @@ public class EndGame : ScriptableObject
     [SerializeField] private Structure _structure;
     [SerializeField] private Currency _currency;
     private GameOverMenuController _gameOverMenu;
-
     public void SetGameOverMenu(GameOverMenuController gameOverMenuController) => _gameOverMenu = gameOverMenuController;
+    
     public void CheckHand(List<Segment> segments, int rerollCostBlood, int rerollCostSteam)
     {
         if (_currency.BloodAmount < rerollCostBlood && !HasOpenSlotOfType(IsSteam)) _gameOverMenu.gameObject.SetActive(true);
         if (_currency.SteamAmount < rerollCostSteam && !HasOpenSlotOfType(IsBlood)) _gameOverMenu.gameObject.SetActive(true);
-        if (CanAffordCard(segments, rerollCostBlood, rerollCostSteam) && HasOpenSlots()) return;
-    
+        if (CanAffordCard(segments, rerollCostBlood, rerollCostSteam) && HasOpenSlotOfType(IsEither)) return;
+        _gameOverMenu.gameObject.SetActive(true);
     }
 
     private bool CanAffordCard(List<Segment> segments, int rerollCostBlood, int rerollCostSteam)
@@ -38,35 +38,18 @@ public class EndGame : ScriptableObject
 
         return false;
     }
-    private bool HasOpenSlots()
-    {
-        foreach (var segment in _structure.Segments)
-        {
-            foreach (var link in segment.GetConnectionPoints())
-            {
-                if (_structure.IsOpenPosition(link)) return true;
-            }
-        }
-        return false;
-    }
     private bool HasOpenSlotOfType(Predicate<ConnectionType> predicate)
     {
         foreach (var segment in _structure.Segments)
         {
             foreach (var link in segment.GetConnectionPointsPlus())
             {
-                if (_structure.IsOpenPosition(link.Item1) && predicate(link.Item2)) 
-                {
-                    Debug.Log(link.Item2);
-                    Debug.Log(predicate(link.Item2));
-                    return true;
-                }
+                if (_structure.IsOpenPosition(link.Item1) && predicate(link.Item2)) return true;              
             }
         }
         return false;
     }
     private bool IsBlood(ConnectionType connection) => connection == ConnectionType.Blood;
     private bool IsSteam(ConnectionType connection) => connection == ConnectionType.Steam;
-    private bool IsAny(ConnectionType connection) => true;
-
+    private bool IsEither(ConnectionType connection) => IsBlood(connection) || IsSteam(connection);
 }
