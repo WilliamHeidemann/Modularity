ec20bc6f4546bb076c570fa7308b234576042a2e
Dexter Reese <Ehlers.Jakob@gmail.com>
2024-12-06 11:08:30 +0100

    JE implemented a check for an edge-case game over state.

--- Assets/Scripts/Runtime/Scriptable Objects/EndGame.cs
+++ Assets/Scripts/Runtime/Scriptable Objects/EndGame.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Runtime.Components.Segments;
 using Runtime.Scriptable_Objects;
@@ -13,10 +14,12 @@ public class EndGame : ScriptableObject
     public void SetGameOverMenu(GameOverMenuController gameOverMenuController) => _gameOverMenu = gameOverMenuController;
     public void CheckHand(List<Segment> segments, int rerollCostBlood, int rerollCostSteam)
     {
+        if (_currency.BloodAmount < rerollCostBlood && !HasOpenSlotOfType(IsSteam)) _gameOverMenu.gameObject.SetActive(true);
+        if (_currency.SteamAmount < rerollCostSteam && !HasOpenSlotOfType(IsBlood)) _gameOverMenu.gameObject.SetActive(true);
         if (CanAffordCard(segments, rerollCostBlood, rerollCostSteam) && HasOpenSlots()) return;
-        
-        _gameOverMenu.gameObject.SetActive(true);
+    
     }
+
     private bool CanAffordCard(List<Segment> segments, int rerollCostBlood, int rerollCostSteam)
     {
         if (_currency.BloodAmount >= rerollCostBlood && _currency.SteamAmount >= rerollCostSteam) return true;
@@ -46,5 +49,24 @@ public class EndGame : ScriptableObject
         }
         return false;
     }
+    private bool HasOpenSlotOfType(Predicate<ConnectionType> predicate)
+    {
+        foreach (var segment in _structure.Segments)
+        {
+            foreach (var link in segment.GetConnectionPointsPlus())
+            {
+                if (_structure.IsOpenPosition(link.Item1) && predicate(link.Item2)) 
+                {
+                    Debug.Log(link.Item2);
+                    Debug.Log(predicate(link.Item2));
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    private bool IsBlood(ConnectionType connection) => connection == ConnectionType.Blood;
+    private bool IsSteam(ConnectionType connection) => connection == ConnectionType.Steam;
+    private bool IsAny(ConnectionType connection) => true;
 
 }
