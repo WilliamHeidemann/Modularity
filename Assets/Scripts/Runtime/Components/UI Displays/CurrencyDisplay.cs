abb560711b2877a8ff2dade3f38d85fee2d37bcc
Tim Bramming <90619736+TimmyTheAverage@users.noreply.github.com>
2024-12-15 21:27:43 +0100

    Displayed update text for blood and steam

--- Assets/Scripts/Runtime/Components/UI Displays/CurrencyDisplay.cs
+++ Assets/Scripts/Runtime/Components/UI Displays/CurrencyDisplay.cs
@@ -2,6 +2,7 @@ using System;
 using Runtime.Scriptable_Objects;
 using TMPro;
 using UnityEngine;
+using DG.Tweening;
 
 namespace Runtime.Components
 {
@@ -10,6 +11,13 @@ namespace Runtime.Components
         [SerializeField] private TextMeshProUGUI _BloodCurrencyText;
         [SerializeField] private TextMeshProUGUI _SteamCurrencyText;
         [SerializeField] private Currency _currency;
+
+        [SerializeField] private float _animationTime = 1f;
+        [SerializeField] private float _moveUpAmount = 30f;
+        [SerializeField] private Transform _bloodTextSpawnPos;
+        [SerializeField] private Transform _steamTextSpawnPos;
+        [SerializeField] private GameObject _bloodUpdateText;
+        [SerializeField] private GameObject _steamUpdateText;
 
         private void Start()
         {
@@ -20,8 +28,52 @@ namespace Runtime.Components
 
         private void UpdateCurrencyText(int bloodAmount, int steamAmount)
         {
+            int bloodDifference = bloodAmount - int.Parse(_BloodCurrencyText.text);
+            int steamDifference = steamAmount - int.Parse(_SteamCurrencyText.text);
+
             _BloodCurrencyText.text = bloodAmount.ToString();
             _SteamCurrencyText.text = steamAmount.ToString();
+
+            if (bloodDifference != 0 && steamDifference != 0)
+            {
+                UpdateTextAnimation(_bloodTextSpawnPos, _bloodUpdateText, bloodDifference);
+                UpdateTextAnimation(_steamTextSpawnPos, _steamUpdateText, steamDifference);
+            }
+            else if (bloodDifference != 0)
+            {
+                UpdateTextAnimation(_bloodTextSpawnPos, _bloodUpdateText, bloodDifference);
+            }
+            else if (steamDifference != 0)
+            {
+                UpdateTextAnimation(_steamTextSpawnPos, _steamUpdateText, steamDifference);
+            }
+        }
+
+        private void UpdateTextAnimation(Transform _spawnPosition, GameObject _updateText, int updateAmount)
+        {
+            GameObject updateTextClone = Instantiate(_updateText, _spawnPosition);
+            updateTextClone.transform.parent = _spawnPosition;
+            TMP_Text updateText = updateTextClone.GetComponent<TMP_Text>();
+            float moveAmount = 0;
+
+            if (updateAmount < 0)
+            {
+                updateText.text = updateAmount.ToString();
+                moveAmount = -_moveUpAmount;
+            }
+            else
+            {
+                updateText.text = updateAmount.ToString();
+                moveAmount = _moveUpAmount;
+            }
+
+            var textFade = updateText.DOFade(0, _animationTime);
+            var moveUp = updateTextClone.transform.DOMoveY(updateTextClone.transform.position.y + moveAmount, _animationTime).SetEase(Ease.OutQuad);
+
+            var sequence = DOTween.Sequence();
+            sequence.Join(textFade);
+            sequence.Join(moveUp);
+            sequence.OnComplete(() => Destroy(updateTextClone));
         }
     }
 }
