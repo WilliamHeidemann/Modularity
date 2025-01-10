62d279428d5b0953ddb07a3f59897b6771d4352e
William Skou Heidemann <williamskou2227@gmail.com>
2025-01-10 13:28:11 +0100

    structures can now be saved and loaded to and from save files

--- Assets/Scripts/Runtime/Components/Utility/DeveloperInputReader.cs
+++ Assets/Scripts/Runtime/Components/Utility/DeveloperInputReader.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Runtime.Components.Segments;
 using Runtime.DataLayer;
@@ -13,18 +14,32 @@ namespace Runtime.Components.Utility
     {
         [SerializeField] private Builder _builder;
         [SerializeField] private Segment _treePrefab;
+        [SerializeField] private Structure _structure;
 
         private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown(KeyCode.J)) SaveStructureToJson();
+            if (Input.GetKeyDown(KeyCode.L)) LoadStructureFromJson();
+            if (Input.GetKeyDown(KeyCode.M)) CreateMegaStructure();
+        }
+
+        private void CreateMegaStructure()
+        {
+            var segments = CreateStructure();
+            foreach (SegmentData segmentData in segments)
             {
-                var segments = CreateStructure();
-                foreach (SegmentData segmentData in segments)
-                {
-                    _builder.BuildInstant(segmentData, _treePrefab);
-                }
-                // SaveStructure();
+                _builder.BuildInstant(segmentData, _treePrefab);
             }
+        }
+
+        private void TearDownStructure()
+        {
+            var segments = FindObjectsByType<Segment>(FindObjectsSortMode.None);
+            for (int i = 0; i < segments.Length; i++)
+            {
+                Destroy(segments[i].gameObject);
+            }
+            _structure.Clear();
         }
 
         private IEnumerable<SegmentData> CreateStructure()
@@ -46,11 +61,62 @@ namespace Runtime.Components.Utility
             }
         }
 
-        // private void SaveStructure()
-        // {
-        //     string json = _structure.ToJson();
-        //     print($"RAW JSON STRING: \n{json}");
-        //     print($"JSON -> structure -> JSON: \n{json.ToStructure().ToJson()}");
-        // }
+        private void SaveStructureToJson()
+        {
+            string json = _structure.ToJson();
+            var filePath = $"{Application.persistentDataPath}/structure.txt";
+        
+            try
+            {
+                using var writer = new StreamWriter(filePath);
+                Debug.Log($"File saved to {filePath}");
+                Debug.Log(json);
+                writer.WriteLine(json);
+            }
+            catch (System.Exception ex)
+            {
+                Debug.LogError("Error writing to file: " + ex.Message);
+            }
+        }
+
+        public void LoadStructureFromJson()
+        {
+            TearDownStructure();
+
+            var segments = Resources.FindObjectsOfTypeAll<Segment>();
+            
+            var filePath = $"{Application.persistentDataPath}/structure.txt";
+            if (!File.Exists(filePath))
+            {
+                Debug.LogError("File does not exist");
+                return;
+            }
+
+            try
+            {
+                using var reader = new StreamReader(filePath);
+                string json = reader.ReadToEnd();
+                var structure = json.ToStructure();
+                _structure.Clear();
+                foreach (SegmentData segmentData in structure.Segments)
+                {
+                    var prefabOption = segments.FirstOption(s => s.StaticSegmentData == segmentData.StaticSegmentData);
+                    if (prefabOption.IsSome(out Segment prefab))
+                    {
+                        _builder.BuildInstant(segmentData, prefab);
+                    }
+                    else
+                    {
+                        Debug.LogError($"Prefab not found for static segment data {segmentData.StaticSegmentData}");
+                    }
+                }
+                Debug.Log("Structure loaded from file");
+                Debug.Log(json);
+            }
+            catch (System.Exception ex)
+            {
+                Debug.LogError("Error reading from file: " + ex.Message);
+            }
+        }
     }
 }
--- Assets/Scripts/Runtime/DataLayer/StaticSegmentData.cs
+++ Assets/Scripts/Runtime/DataLayer/StaticSegmentData.cs
@@ -33,5 +33,45 @@ namespace Runtime.DataLayer
             false when IsSteam => SoundFX.SteamConnectorPlacement,
             _ => throw new ArgumentOutOfRangeException($"Segment is neither blood nor steam: {name}")
         };
+        
+        protected bool Equals(StaticSegmentData other)
+        {
+            return base.Equals(other) && ConnectionPoints.Equals(other.ConnectionPoints) && IsBlood == other.IsBlood && IsSteam == other.IsSteam && IsSource == other.IsSource && BloodReward == other.BloodReward && SteamReward == other.SteamReward && BloodCost == other.BloodCost && SteamCost == other.SteamCost && BloodRequirements == other.BloodRequirements && SteamRequirements == other.SteamRequirements;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((StaticSegmentData)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = new HashCode();
+            hashCode.Add(base.GetHashCode());
+            hashCode.Add(ConnectionPoints);
+            hashCode.Add(IsBlood);
+            hashCode.Add(IsSteam);
+            hashCode.Add(IsSource);
+            hashCode.Add(BloodReward);
+            hashCode.Add(SteamReward);
+            hashCode.Add(BloodCost);
+            hashCode.Add(SteamCost);
+            hashCode.Add(BloodRequirements);
+            hashCode.Add(SteamRequirements);
+            return hashCode.ToHashCode();
+        }
+
+        public static bool operator ==(StaticSegmentData left, StaticSegmentData right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(StaticSegmentData left, StaticSegmentData right)
+        {
+            return !Equals(left, right);
+        }
     }
 }
