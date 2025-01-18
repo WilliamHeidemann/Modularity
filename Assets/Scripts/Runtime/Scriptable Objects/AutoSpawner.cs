bd786b2d67a3529e1ec0e859be17fa8ae954326a
William Skou Heidemann <williamskou2227@gmail.com>
2025-01-18 15:31:26 +0100

    made it so sources can be spawned when we need them for the trailer

--- Assets/Scripts/Runtime/Scriptable Objects/AutoSpawner.cs
+++ Assets/Scripts/Runtime/Scriptable Objects/AutoSpawner.cs
@@ -28,8 +28,20 @@ namespace Runtime.Scriptable_Objects
         [SerializeField] private float _distancePercentage;
         [SerializeField] private Collectable _collectablePrefab;
         private readonly List<Collectable> _collectables = new();
+
         public delegate void CollectedCollectables(int collectedAmount);
+
         public static event CollectedCollectables OnCollectedCollectables;
+
+        [Space]
+        [Header("FIXED SOURCE SPAWN POSITIONS")]
+        [SerializeField] private bool ShouldSpawnInPredefinedPosition;
+        [Header("Heart")]
+        [SerializeField] private Vector3Int HeartSpawnPosition;
+        [SerializeField] private Vector3 HeartRotation;
+        [Header("Furnace")]
+        [SerializeField] private Vector3Int FurnaceSpawnPosition;
+        [SerializeField] private Vector3 FurnaceRotation;
 
         public void Clear()
         {
@@ -47,7 +59,7 @@ namespace Runtime.Scriptable_Objects
             {
                 SoundFXPlayer.Instance.Play(SoundFX.OrbCollected);
             }
-            
+
             _questFactory.CollectableCollected(collectables.Count);
             OnCollectedCollectables?.Invoke(collectables.Count);
             collectables.ForEach(c => _currencyPopup.GainCurrency(c.Position, c.StaticSegmentData));
@@ -55,12 +67,39 @@ namespace Runtime.Scriptable_Objects
             collectables.ForEach(c => Destroy(c.gameObject));
         }
 
-        public void SpawnBloodSource() => SpawnRandomSource(_bloodSource);
-        public void SpawnSteamSource() => SpawnRandomSource(_steamSource);
+        public void SpawnBloodSource()
+        {
+            if (ShouldSpawnInPredefinedPosition)
+            {
+                SpawnSource(_bloodSource, HeartSpawnPosition, Quaternion.Euler(HeartRotation));
+            }
+            else
+            {
+                SpawnRandomSource(_bloodSource);
+            }
+        }
+
+        public void SpawnSteamSource()
+        {
+            if (ShouldSpawnInPredefinedPosition)
+            {
+                SpawnSource(_steamSource, FurnaceSpawnPosition, Quaternion.Euler(FurnaceRotation));
+            }
+            else
+            {
+                SpawnRandomSource(_steamSource);
+            }
+        }
 
         private void SpawnRandomSource(Segment source)
         {
-            var spawnPosition = SpawnUtility.Get(GetSpawnPosition, _structure.IsValidSourcePlacement);
+            Vector3Int spawnPosition = SpawnUtility.Get(GetSpawnPosition, _structure.IsValidSourcePlacement);
+            Quaternion spawnRotation = GetRandomRotation();
+            SpawnSource(source, spawnPosition, spawnRotation);
+        }
+
+        private void SpawnSource(Segment source, Vector3Int spawnPosition, Quaternion rotation)
+        {
             _selection.Reset();
             _selection.Prefab = Option<Segment>.Some(source);
             _builder.Build(spawnPosition, GetRandomRotation(), true);
