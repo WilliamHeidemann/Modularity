a83b15d25c5740e7c1a52986889b9a2d24208c29
William Skou Heidemann <williamskou2227@gmail.com>
2025-04-25 13:02:58 +0200

    integrate metal slot visualizer

--- Assets/Scripts/Runtime/Scriptable Objects/SlotVisualizer.cs
+++ Assets/Scripts/Runtime/Scriptable Objects/SlotVisualizer.cs
@@ -9,17 +9,27 @@ namespace Runtime.Scriptable_Objects
     [CreateAssetMenu]
     public class SlotVisualizer : ScriptableObject
     {
-        [SerializeField] private GameObject _slotPrefab;
+        [SerializeField] private GameObject _fleshSlotPrefab;
+        [SerializeField] private GameObject _metalSlotPrefab;
+        
         [SerializeField] private Structure _structure;
-        private ObjectPool<GameObject> _objectPool;
-        private readonly List<GameObject> _slots = new();
-
+        private ObjectPool<GameObject> _fleshPool;
+        private ObjectPool<GameObject> _metalPool;
+        private readonly List<GameObject> _fleshSlots = new();
+        private readonly List<GameObject> _metalSlots = new();
 
         public void Initialize()
         {
-            _slots.Clear();
-            _objectPool = new ObjectPool<GameObject>(
-                createFunc: () => Instantiate(_slotPrefab),
+            _fleshSlots.Clear();
+            _fleshPool = new ObjectPool<GameObject>(
+                createFunc: () => Instantiate(_fleshSlotPrefab),
+                actionOnGet: obj => obj.SetActive(true),
+                actionOnRelease: obj => obj.SetActive(false),
+                actionOnDestroy: obj => obj.SetActive(false)
+            );
+            
+            _metalPool = new ObjectPool<GameObject>(
+                createFunc: () => Instantiate(_metalSlotPrefab),
                 actionOnGet: obj => obj.SetActive(true),
                 actionOnRelease: obj => obj.SetActive(false),
                 actionOnDestroy: obj => obj.SetActive(false)
@@ -31,20 +41,42 @@ namespace Runtime.Scriptable_Objects
             var positionAndRotations = _structure.GetOpenSlots(connectionType);
             foreach (var (position, rotation) in positionAndRotations)
             {
-                var slot = _objectPool.Get();
+                var slot = connectionType switch
+                {
+                    ConnectionType.None => throw new ArgumentOutOfRangeException(nameof(connectionType), connectionType, null),
+                    ConnectionType.Blood => _fleshPool.Get(),
+                    ConnectionType.Steam => _metalPool.Get(),
+                    _ => throw new ArgumentOutOfRangeException(nameof(connectionType), connectionType, null)
+                };
+                
                 slot.transform.position = position;
                 slot.transform.rotation = rotation;
-                _slots.Add(slot);
+                
+                var slots = connectionType switch
+                {
+                    ConnectionType.None => throw new ArgumentOutOfRangeException(nameof(connectionType), connectionType, null),
+                    ConnectionType.Blood => _fleshSlots,
+                    ConnectionType.Steam => _metalSlots,
+                    _ => throw new ArgumentOutOfRangeException(nameof(connectionType), connectionType, null)
+                };
+                
+                slots.Add(slot);
             }
         }
 
         public void HideSlots()
         {
-            foreach (GameObject slot in _slots)
+            foreach (GameObject slot in _fleshSlots)
             {
-                _objectPool.Release(slot);
+                _fleshPool.Release(slot);
             }
-            _slots.Clear();
+            _fleshSlots.Clear();
+            
+            foreach (GameObject slot in _metalSlots)
+            {
+                _metalPool.Release(slot);
+            }
+            _metalSlots.Clear();
         }
     }
 }
