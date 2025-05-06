53707225ac55b0fa351138dbe36a2e3c076fb770
William Skou Heidemann <williamskou2227@gmail.com>
2025-05-06 10:18:41 +0200

    google forms button integrated

--- Assets/Scripts/Runtime/UnityCloud/GoogleFormsIntegration.cs
+++ Assets/Scripts/Runtime/UnityCloud/GoogleFormsIntegration.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 
 namespace Runtime.UnityCloud
 {
-    public static class GoogleFormsIntegration
+    public class GoogleFormsIntegration : MonoBehaviour
     {
         public static void SendDataToGoogleForms()
         {
-            var sessionId = AnalyticsService.Instance.SessionID;
-            string url = $"https://docs.google.com/forms/d/e/1FAIpQLSeVBfEdKMv37HsFFF0UjiTGlntL8Nc6qWqGJKtxo56_O2LqzQ/viewform?entry.1002212484={sessionId}";
+            string sessionId = AnalyticsService.Instance.SessionID;
+            var url = $"https://docs.google.com/forms/d/e/1FAIpQLSeVBfEdKMv37HsFFF0UjiTGlntL8Nc6qWqGJKtxo56_O2LqzQ/viewform?entry.1002212484={sessionId}";
             Application.OpenURL(url);
         }
     }
